3d29a51f925ae681efbf811ce6dbb37b78882db1
Kuro <yoyh9ab@outlook.jp>
2022-09-27 16:11:46 +0900

    refactor: Sample code.

--- Prism.CommonDialogPack-Sample-MahApps/ViewModels/MainWindowViewModel.cs
+++ Prism.CommonDialogPack-Sample-MahApps/ViewModels/MainWindowViewModel.cs
@@ -27,14 +27,14 @@ namespace Prism.CommonDialogPack_Sample_MahApps.ViewModels
 
         public ReactiveProperty<string> ResultMessage { get; } = new ReactiveProperty<string>();
 
-        public ReactiveCommand ShowNotificationDialog { get; } = new ReactiveCommand();
-        public ReactiveCommand ShowConfirmationDialog { get; } = new ReactiveCommand();
-        public ReactiveCommand ShowSingleFolderSelectDialog { get; } = new ReactiveCommand();
-        public ReactiveCommand ShowMultiFolderSelectDialog { get; } = new ReactiveCommand();
-        public ReactiveCommand ShowSingleFileSelectDialog { get; } = new ReactiveCommand();
-        public ReactiveCommand ShowMultiFileSelectDialog { get; } = new ReactiveCommand();
-        public ReactiveCommand ShowFileSaveDialog { get; } = new ReactiveCommand();
-        public ReactiveCommand ShowCustomizedFileSaveDialog { get; } = new ReactiveCommand();
+        public ReactiveCommand ShowNotificationDialogCommand { get; } = new ReactiveCommand();
+        public ReactiveCommand ShowConfirmationDialogCommand { get; } = new ReactiveCommand();
+        public ReactiveCommand ShowSingleFolderSelectDialogCommand { get; } = new ReactiveCommand();
+        public ReactiveCommand ShowMultiFolderSelectDialogCommand { get; } = new ReactiveCommand();
+        public ReactiveCommand ShowSingleFileSelectDialogCommand { get; } = new ReactiveCommand();
+        public ReactiveCommand ShowMultiFileSelectDialogCommand { get; } = new ReactiveCommand();
+        public ReactiveCommand ShowFileSaveDialogCommand { get; } = new ReactiveCommand();
+        public ReactiveCommand ShowCustomizedFileSaveDialogCommand { get; } = new ReactiveCommand();
         public ReactiveCommand ShowProgressDialogCommand { get; } = new ReactiveCommand();
         public ReactiveCommand ShowIndeterminateProgreesDialogCommand { get; } = new ReactiveCommand();
 
@@ -45,143 +45,180 @@ namespace Prism.CommonDialogPack_Sample_MahApps.ViewModels
         {
             this.dialogService = dialogService;
             this.eventAggregator = eventAggregator;
-            this.ShowNotificationDialog.Subscribe(() =>
-            {
-                // Standard
-                //var param = new DialogParameters
-                //{
-                //    { DialogParameterNames.Message, "Notification" },
-                //    { DialogParameterNames.Title, "Notification" },
-                //    { DialogParameterNames.WindowStyle, (System.Windows.Style)App.Current.FindResource("dialogStyle") },
-                //};
-                //this.dialogService.ShowDialog(DialogNames.Notification, param, res => this.ResultMessage.Value = "Notification");
+            this.ShowNotificationDialogCommand.Subscribe(this.ShowNotificationDialog);
+            this.ShowConfirmationDialogCommand.Subscribe(this.ShowConfirmationDialog);
+            this.ShowSingleFolderSelectDialogCommand.Subscribe(this.ShowSingleFolderSelectDialog);
+            this.ShowMultiFolderSelectDialogCommand.Subscribe(this.ShowMultiFolderSelectDialog);
+            this.ShowSingleFileSelectDialogCommand.Subscribe(this.ShowSingleFileSelectDialog);
+            this.ShowMultiFileSelectDialogCommand.Subscribe(this.ShowMultiFileSelectDialog);
+            this.ShowFileSaveDialogCommand.Subscribe(this.ShowFileSaveDialog);
+            this.ShowCustomizedFileSaveDialogCommand.Subscribe(this.ShowCustomizedFileSaveDialog);
+            this.ShowProgressDialogCommand.Subscribe(this.ShowProgressDialog);
+            this.ShowIndeterminateProgreesDialogCommand.Subscribe(this.ShowIndeterminateProgreesDialog);
+        }
+        /// <summary>
+        /// Show NotificationDialog.
+        /// </summary>
+        private void ShowNotificationDialog()
+        {
+            // Standard
+            //var param = new DialogParameters
+            //{
+            //    { DialogParameterNames.Message, "Notification" },
+            //    { DialogParameterNames.Title, "Notification" },
+            //    { DialogParameterNames.WindowStyle, (System.Windows.Style)App.Current.FindResource("dialogStyle") },
+            //};
+            //this.dialogService.ShowDialog(DialogNames.Notification, param, res => this.ResultMessage.Value = "Notification");
 
-                // Extensions
-                this.dialogService.ShowNotification("Notification", "Notification", res => this.ResultMessage.Value = "Notification");
-            });
-            this.ShowConfirmationDialog.Subscribe(() =>
+            // Extensions
+            this.dialogService.ShowNotification("Notification", "Notification", res => this.ResultMessage.Value = "Notification");
+        }
+        /// <summary>
+        /// Show ConfirmationDialog.
+        /// </summary>
+        private void ShowConfirmationDialog()
+        {
+            this.dialogService.ShowConfirmation("Confirmation?", "Confirmation", res =>
             {
-                this.dialogService.ShowConfirmation("Confirmation?", "Confirmation", res =>
+                if (res.Result == ButtonResult.OK)
                 {
-                    if (res.Result == ButtonResult.OK)
-                        this.ResultMessage.Value = "Confirmed OK";
-                    else if (res.Result == ButtonResult.Cancel)
-                        this.ResultMessage.Value = "Confirmed Cancel";
-                    else
-                        this.ResultMessage.Value = $"Confirmed {res.Result.ToString()}";
-                });
+                    this.ResultMessage.Value = "Confirmed OK";
+                }
+                else if (res.Result == ButtonResult.Cancel)
+                {
+                    this.ResultMessage.Value = "Confirmed Cancel";
+                }
+                else
+                {
+                    this.ResultMessage.Value = $"Confirmed {res.Result}";
+                }
             });
-            this.ShowSingleFolderSelectDialog.Subscribe(() =>
+        }
+        /// <summary>
+        /// Show SingleFolderSelectDialog.
+        /// </summary>
+        private void ShowSingleFolderSelectDialog()
+        {
+            this.dialogService.ShowFolderSelectDialog("SingleFolderSelect", false, res =>
             {
-                this.dialogService.ShowFolderSelectDialog("SingleFolderSelect", false, res =>
+                if (res.Result == ButtonResult.OK)
                 {
-                    if (res.Result == ButtonResult.OK)
-                    {
-                        var selectedPath = res.Parameters.GetValue<IEnumerable<string>>(DialogResultParameterNames.SelectedPaths).First();
-                        this.ResultMessage.Value = $"Selected Folder: {selectedPath}";
-                    }
-                    else
-                    {
-                        this.ResultMessage.Value = "Cancel Single Folder Select";
-                    }
-                });
+                    var selectedPath = res.Parameters.GetValue<IEnumerable<string>>(DialogResultParameterNames.SelectedPaths).First();
+                    this.ResultMessage.Value = $"Selected Folder: {selectedPath}";
+                }
+                else
+                {
+                    this.ResultMessage.Value = "Cancel Single Folder Select";
+                }
             });
-            this.ShowMultiFolderSelectDialog.Subscribe(() =>
+        }
+        /// <summary>
+        /// Show MultiFolderSelectDialog.
+        /// </summary>
+        private void ShowMultiFolderSelectDialog()
+        {
+            this.dialogService.ShowFolderSelectDialog("MultiFolderSelect", true, res =>
             {
-                this.dialogService.ShowFolderSelectDialog("MultiFolderSelect", true, res =>
+                if (res.Result == ButtonResult.OK)
                 {
-                    if (res.Result == ButtonResult.OK)
-                    {
-                        var selectedPaths = res.Parameters.GetValue<IEnumerable<string>>(DialogResultParameterNames.SelectedPaths);
-                        this.ResultMessage.Value = $"Selected Folders:{Environment.NewLine}    {string.Join($"{Environment.NewLine}    ", selectedPaths)}";
-                    }
-                    else
-                    {
-                        this.ResultMessage.Value = "Cancel Multi Folder Select";
-                    }
-                });
+                    var selectedPaths = res.Parameters.GetValue<IEnumerable<string>>(DialogResultParameterNames.SelectedPaths);
+                    this.ResultMessage.Value = $"Selected Folders:{Environment.NewLine}    {string.Join($"{Environment.NewLine}    ", selectedPaths)}";
+                }
+                else
+                {
+                    this.ResultMessage.Value = "Cancel Multi Folder Select";
+                }
             });
-            this.ShowSingleFileSelectDialog.Subscribe(() =>
+        }
+        /// <summary>
+        /// Show SingleFileSelectDialog.
+        /// </summary>
+        private void ShowSingleFileSelectDialog()
+        {
+            this.dialogService.ShowFileSelectDialog("SingleFileSelect", false, res =>
             {
-                this.dialogService.ShowFileSelectDialog("SingleFileSelect", false, res =>
+                if (res.Result == ButtonResult.OK)
                 {
-                    if (res.Result == ButtonResult.OK)
-                    {
-                        var selectedPaths = res.Parameters.GetValue<IEnumerable<string>>(DialogResultParameterNames.SelectedPaths);
-                        if (selectedPaths != null && selectedPaths.Any())
-                            this.ResultMessage.Value = $"Selected File: {selectedPaths.First()}";
-                        else
-                            this.ResultMessage.Value = "File Not Selected";
-                    }
-                    else
-                    {
-                        this.ResultMessage.Value = "Cancel Single File Select";
-                    }
-                });
+                    var selectedPaths = res.Parameters.GetValue<IEnumerable<string>>(DialogResultParameterNames.SelectedPaths);
+                    this.ResultMessage.Value = selectedPaths != null && selectedPaths.Any() ? $"Selected File: {selectedPaths.First()}" : "File Not Selected";
+                }
+                else
+                {
+                    this.ResultMessage.Value = "Cancel Single File Select";
+                }
             });
-            this.ShowMultiFileSelectDialog.Subscribe(() =>
+        }
+        /// <summary>
+        /// Show MultiFileSelectDialog.
+        /// </summary>
+        private void ShowMultiFileSelectDialog()
+        {
+            var filters = new[]
             {
-                var filters = new[]
+                new FileFilter("Text File (*.txt; *.csv)", new[] { ".txt", ".csv" }),
+                new FileFilter("All Files (*.*)"),
+                new FileFilter("Excel File (*.xlsx; *.xlsm; *.xls)", ".xlsx", ".xlsm", ".xls"),
+            };
+            this.dialogService.ShowFileSelectDialog("MultiFileSelect", true, res =>
+            {
+                if (res.Result == ButtonResult.OK)
                 {
-                    new FileFilter("Text File (*.txt; *.csv)", new[] { ".txt", ".csv" }),
-                    new FileFilter("All Files (*.*)"),
-                    new FileFilter("Excel File (*.xlsx; *.xlsm; *.xls)", ".xlsx", ".xlsm", ".xls"),
-                };
-                this.dialogService.ShowFileSelectDialog("MultiFileSelect", true, res =>
+                    var selectedPaths = res.Parameters.GetValue<IEnumerable<string>>(DialogResultParameterNames.SelectedPaths);
+                    this.ResultMessage.Value = selectedPaths != null && selectedPaths.Any()
+                        ? $"Selected Files:{Environment.NewLine}    {string.Join($"{Environment.NewLine}    ", selectedPaths)}"
+                        : "File Not Selected";
+                }
+                else
                 {
-                    if (res.Result == ButtonResult.OK)
-                    {
-                        var selectedPaths = res.Parameters.GetValue<IEnumerable<string>>(DialogResultParameterNames.SelectedPaths);
-                        if (selectedPaths != null && selectedPaths.Any())
-                            this.ResultMessage.Value = $"Selected Files:{Environment.NewLine}    {string.Join($"{Environment.NewLine}    ", selectedPaths)}";
-                        else
-                            this.ResultMessage.Value = "File Not Selected";
-                    }
-                    else
-                    {
-                        this.ResultMessage.Value = "Cancel Multi File Select";
-                    }
-                },
-                // Add File Filters
-                filters: filters);
+                    this.ResultMessage.Value = "Cancel Multi File Select";
+                }
+            },
+            // Add File Filters
+            filters: filters);
+        }
+        /// <summary>
+        /// Show FileSaveDialog.
+        /// </summary>
+        private void ShowFileSaveDialog()
+        {
+            this.dialogService.ShowFileSaveDialog("FileSave", res =>
+            {
+                if (res.Result == ButtonResult.OK)
+                {
+                    var saveFilePath = res.Parameters.GetValue<string>(DialogResultParameterNames.SaveFilePath);
+                    this.ResultMessage.Value = $"Save File Path: {saveFilePath}";
+                }
+                else
+                {
+                    this.ResultMessage.Value = "Cancel File Save";
+                }
             });
-            this.ShowFileSaveDialog.Subscribe(() =>
+        }
+        /// <summary>
+        /// Show CustomizedFileSaveDialog.
+        /// </summary>
+        private void ShowCustomizedFileSaveDialog()
+        {
+            var textResource = new ExplorerBaseTextResource()
             {
-                this.dialogService.ShowFileSaveDialog("FileSave", res =>
-                {
-                    if (res.Result == ButtonResult.OK)
-                    {
-                        var saveFilePath = res.Parameters.GetValue<string>(DialogResultParameterNames.SaveFilePath);
-                        this.ResultMessage.Value = $"Save File Path: {saveFilePath}";
-                    }
-                    else
-                    {
-                        this.ResultMessage.Value = "Cancel File Save";
-                    }
-                });
-            });
-            this.ShowCustomizedFileSaveDialog.Subscribe(() =>
+                FileName = "Name",
+                FileDateModified = "Date modified",
+                FileType = "Type",
+                FileSize = "Size",
+            };
+            var filters = new[]
             {
-                var textResource = new ExplorerBaseTextResource()
-                {
-                    FileName = "Name",
-                    FileDateModified = "Date modified",
-                    FileType = "Type",
-                    FileSize = "Size",
-                };
-                var filters = new[]
-                {
-                    new FileFilter("All Files (*.*)"),
-                    new FileFilter("Text File (*.txt)", ".txt"),
-                    new FileFilter("CSV File (*.csv)",".csv"),
-                };
-                var icons = new ExplorerIcons()
-                {
-                    BackWardIcon = new BitmapImage(new Uri("/Resources/Backwards_16x.png", UriKind.Relative)),
-                    ForwardIcon = new BitmapImage(new Uri("/Resources/Forwards_16x.png", UriKind.Relative)),
-                };
-                Func<string, string> overwriteConfirmationMessageFunc = (x => $"{x} already exists.{Environment.NewLine}Do you want to replace it?");
-                var param = new DialogParameters
+                new FileFilter("All Files (*.*)"),
+                new FileFilter("Text File (*.txt)", ".txt"),
+                new FileFilter("CSV File (*.csv)",".csv"),
+            };
+            var icons = new ExplorerIcons()
+            {
+                BackWardIcon = new BitmapImage(new Uri("/Resources/Backwards_16x.png", UriKind.Relative)),
+                ForwardIcon = new BitmapImage(new Uri("/Resources/Forwards_16x.png", UriKind.Relative)),
+            };
+            Func<string, string> overwriteConfirmationMessageFunc = (x => $"{x} already exists.{Environment.NewLine}Do you want to replace it?");
+            var param = new DialogParameters
                 {
                     { DialogParameterNames.Title, "FileSave" },
                     { DialogParameterNames.DefaultSaveFileName, "Sample.txt" },
@@ -198,22 +235,20 @@ namespace Prism.CommonDialogPack_Sample_MahApps.ViewModels
                     { DialogParameterNames.OverwriteConfirmationCancelButtonText, "No" },
                     //{ DialogParameterNames.RootFolders, new [] { @"C:\" } },
                 };
-                this.dialogService.ShowDialog(DialogNames.FileSaveDialog, param, res =>
+            this.dialogService.ShowDialog(DialogNames.FileSaveDialog, param, res =>
+            {
+                if (res.Result == ButtonResult.OK)
                 {
-                    if (res.Result == ButtonResult.OK)
-                    {
-                        var saveFilePath = res.Parameters.GetValue<string>(DialogResultParameterNames.SaveFilePath);
-                        this.ResultMessage.Value = $"Save File Path: {saveFilePath}";
-                    }
-                    else
-                    {
-                        this.ResultMessage.Value = "Cancel File Save";
-                    }
-                });
+                    var saveFilePath = res.Parameters.GetValue<string>(DialogResultParameterNames.SaveFilePath);
+                    this.ResultMessage.Value = $"Save File Path: {saveFilePath}";
+                }
+                else
+                {
+                    this.ResultMessage.Value = "Cancel File Save";
+                }
             });
-            this.ShowProgressDialogCommand.Subscribe(ShowProgressDialog);
-            this.ShowIndeterminateProgreesDialogCommand.Subscribe(this.ShowIndeterminateProgreesDialog);
         }
+
         /// <summary>
         /// Task to publish <see cref="ProgressEvent"/>.
         /// </summary>
